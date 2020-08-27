0a2b57b46b1514c3e25174fcbaf468e8d6eccc76
thomas royal <thomas.m.royal@gmail.com>
2020-08-27 19:28:57 -0400

    intial scaffolding of Key class

--- src/key.js
+++ src/key.js
@@ -1,19 +1,26 @@
 function noop(){}
 
 export default class Key {
-  constructor(props){
-    props = props || {};
-    this.midiNumber = props.midiNumber || 0;
-    this.cssPrefix = props.cssPrefix || "";
-    this.keyOn = props.keyOn || noop;
-    this.keyOff = props.keyOff || noop;
+  constructor(element, ns, props){
+    if (!props || !element || !ns){
+      throw '"Key" object requires propeties, element, and namespace';
+    } else {
+      // TODO: throw error if keys are missing
+      this.midiNumber = props.midiNumber || 0;
+      this.cssInactiveName = props.cssInactiveName || "svgkey_off";
+      this.cssActiveName = props.cssActiveName || "svgkey_on";
+      this.keyOn = props.keyOn || noop;
+      this.keyOff = props.keyOff || noop;
 
-    this.draw();
+      this.setObj(element, ns, props);
+    }
   }
 
-  draw(){
-    console.log("draw functions here");
+  draw(element, ns, props){
+    this.rect = document.createElementNS(ns, 'rect');
+    this.rect.setAttributeNS(null, 'width', props.width || 0);
+    this.rect.setAttributeNS(null, 'height', props.height || 0);
+    this.rect.className.baseVal = this.cssInactiveName;
   }
-
 
 } 
